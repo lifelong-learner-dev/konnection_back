3b5a3dcc3f30dfbcc838dc9cf89ddd3bad6bcf21
lifelong-learner-dev <dev.lifelong.learner.dev@gmail.com>
2024-07-25 20:49:17 +0900

    gemini chatbot

--- konnection/views.py
+++ konnection/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import google.generativeai as genai
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 genai.configure(api_key=settings.GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 
+@csrf_exempt
 def chat_response(request):
     if request.method == "POST":
         message = request.POST.get("message")
